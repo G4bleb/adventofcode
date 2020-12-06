1348a3bcef70a6b232e8fdac6080716cd15972e4
g4bleb <gableb@hotmail.fr>
2020-12-06 13:48:04 +0100

    Day 6

---
+++ 6.py
@@ -0,0 +1,26 @@
+import string
+import re
+
+
+def resolvePart1():
+    count = 0
+    f = open('6', 'r')
+    paragraphs = f.read().split('\n\n')
+    for paragraph in paragraphs:
+        paragraph = paragraph.replace('\n', '')
+        count += len(list(dict.fromkeys(paragraph)))
+    return count
+
+
+def resolvePart2():
+    count = 0
+    f = open('6', 'r')
+    paragraphs = f.read().split('\n\n')
+    for paragraph in paragraphs:
+        n_people = paragraph.count('\n') + 1
+        for letter in string.ascii_lowercase:
+            count += n_people == len(re.findall(f"[{letter}]", paragraph))
+    return count
+
+
+print(resolvePart1())
