8da6bc9d070adbca1d844d06e4018e37ce061ee4
Gabriel Lebis <gabriel.lebis@genesys.com>
2022-12-13 10:03:37 +0100

    2022 day 13

---
+++ 2022/13/13.py
@@ -0,0 +1,63 @@
+import json
+from functools import cmp_to_key
+
+
+def compare(l, r):
+    if isinstance(l, int) and isinstance(r, int):
+        if l == r:
+            return 'continue'
+        return -1 if l < r else 1
+    if isinstance(l, list) and isinstance(r, list):
+        for lv, rv in zip(l, r):
+            comparison = compare(lv, rv)
+            if comparison != 'continue':
+                return comparison
+        if len(r) != len(l):
+            return -1 if len(r) > len(l) else 1
+        return 'continue'
+    # One is an integer
+    if isinstance(l, int):
+        l = [l]
+    else:
+        r = [r]
+    return compare(l, r)
+
+
+def solve1():
+    packet_pair: list[list] = [[],  []]
+    i = 0
+    res = 0
+    with open('input.txt', 'r') as f:
+        while line := f.readline():
+            i += 1
+            packet_pair[0] = json.loads(line)
+            packet_pair[1] = json.loads(f.readline())
+            left = packet_pair[0]
+            right = packet_pair[1]
+            next(f, False)
+            if compare(left, right) == -1:
+                res += i
+    print(res)
+
+
+def solve2():
+    packets = []
+    with open('input.txt', 'r') as f:
+        while line := f.readline():
+            line = line.rstrip()
+            if line:
+                packets.append(json.loads(line))
+    packets.append([[2]])
+    packets.append([[6]])
+    packets.sort(key=cmp_to_key(compare))
+    res = 1
+    i = 0
+    for p in packets:
+        i += 1
+        if p == [[2]] or p == [[6]]:
+            res *= i
+    print(res)
+
+
+solve1()
+solve2()
